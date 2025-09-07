c54159b565c9430bcc76b55d6e858b81db76298f
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-09-08 01:21:47 +0300

    Expo Router, React Components, and NativeWind Integration in React Native

---
+++ prodev-mobile-app-0x05/app/inbox.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const Inbox = () => (
+    <View>
+        <Text>Inbox Screen</Text>
+    </View>
+);
+
+export default Inbox;
---
+++ prodev-mobile-app-0x05/app/join.tsx
@@ -0,0 +1,70 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { useRouter } from "expo-router";
+import { styles } from "../styles/_joinstyle";
+import { GOOGLELOGO, FACEBOOKLOGO } from "../constants";
+
+export default function Signin() {
+    const router = useRouter();
+
+    return (
+        <View style={styles.container}>
+            {/* Title */}
+            <View style={styles.titleTextGroup}>
+                <Text style={styles.titleText}>Sign In</Text>
+                <Text style={styles.subText}>Welcome back! Please enter your details</Text>
+            </View>
+
+            {/* Form */}
+            <View style={styles.formGroup}>
+                <View>
+                    <Text style={styles.formLabel}>Email</Text>
+                    <TextInput placeholder="Enter your email" style={styles.formControl} />
+                </View>
+
+                <View>
+                    <Text style={styles.formLabel}>Password</Text>
+                    <View style={styles.formPasswordControl}>
+                        <TextInput
+                            placeholder="Enter your password"
+                            secureTextEntry
+                            style={styles.passwordControl}
+                        />
+                    </View>
+                </View>
+
+                <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+
+                <TouchableOpacity style={styles.primaryButton}>
+                    <Text style={styles.buttonText}>Sign In</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* Social buttons */}
+            <View style={styles.dividerGroup}>
+                <View style={styles.divider} />
+                <Text style={styles.dividerText}>or continue with</Text>
+                <View style={styles.divider} />
+            </View>
+
+            <View style={styles.secondaryButtonGroup}>
+                <TouchableOpacity style={styles.secondaryButton}>
+                    <Image source={GOOGLELOGO} />
+                    <Text style={styles.secondaryButtonText}>Google</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.secondaryButton}>
+                    <Image source={FACEBOOKLOGO} />
+                    <Text style={styles.secondaryButtonText}>Facebook</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* Signup link */}
+            <View style={styles.signupgroup}>
+                <Text style={styles.signupTitleText}>Don’t have an account? </Text>
+                <TouchableOpacity onPress={() => router.push("/join")}>
+                    <Text style={styles.signupSubTitleText}>Join Now</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
